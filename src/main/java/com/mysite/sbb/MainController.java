2193681cb520ddb56b43807ab42544f058eb8703
FullSoYu <hey6410@naver.com>
2022-10-27 18:45:59 +0900

    jpa 연습중

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -12,7 +12,7 @@ public class MainController {
     @RequestMapping("/sbb")
     @ResponseBody
     public String index() {
-        System.out.println("sbb");
+        System.out.println("정상 동작");
         return "정상 출력";
     }
 
@@ -20,21 +20,31 @@ public class MainController {
     @ResponseBody
     public String showPage1() {
         return """
-                <form method = "POST" action="/page2">
+                <form method = "POST" action="/page1">
                     <input type='number' />
-                    <input type = "submit" value = "page2로 POST방식으로 이동" />
+                    <input type = "submit" value = "page1로 POST방식으로 이동" />
                 </form>
                 """;
 
 
     }
 
-    @PostMapping("/page2")
+    @PostMapping("/page1")
     @ResponseBody
     public String showPage2() {
         return """
+                <form method = "POST" action= "/page3">
                 <h1> 입력된 나이 : %d</h1>
-                <h1> POST 방식으로 옴 </h1>   
+                <h1> POST 방식으로 옴 </h1>
+                <input type = "submit" value = "page3로 POST방식으로 이동" />
+                </form>   
+                """;
+    }
+    @PostMapping("/page3")
+    @ResponseBody
+    public String showPage3() {
+        return """
+                <h1> POST방식(2)으로옴</h1>
                 """;
     }
 
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,64 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.dao.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해 할고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
+	}
+	@Test
+	void testJpa2() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(7, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+
+	@Test
+	void findbyID() {
+		Optional<Question> oq = questionRepository.findById(1);
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+	@Test
+	void findByContent() {
+		Optional<Question> oq = questionRepository.findById(1);
+
+		System.out.println("oq : " + oq);
 	}
 
 }
