f37c182179e4ce3ced5b37fde1e5a0d8dc1c0336
FullSoYu <hey6410@naver.com>
2022-10-27 13:35:04 +0900

    get post

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,6 +1,8 @@
 package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,4 +15,28 @@ public class MainController {
         System.out.println("sbb");
         return "정상 출력";
     }
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showPage1() {
+        return """
+                <form method = "POST" action="/page2">
+                    <input type='number' />
+                    <input type = "submit" value = "page2로 POST방식으로 이동" />
+                </form>
+                """;
+
+
+    }
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2() {
+        return """
+                <h1> 입력된 나이 : %d</h1>
+                <h1> POST 방식으로 옴 </h1>   
+                """;
+    }
+
+
 }
