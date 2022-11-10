45ca531ed1b9b13c0ccce5a2e9fe43d2e8ca2078
FullSoYu <hey6410@naver.com>
2022-11-10 15:43:52 +0900

    return to sbb3 Jump to spring boot 2-07

--- src/main/java/com/mysite/sbb/question/Controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/Controller/QuestionController.java
@@ -1,16 +1,25 @@
 package com.mysite.sbb.question.Controller;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.dao.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/question")
 @Controller
 public class QuestionController {
 
-    @RequestMapping("/question/list")
-    @ResponseBody
+    private final QuestionRepository questionRepository;
 
-    public String list() {
-        return "question list";
+    @RequestMapping("/list")
+    public String list(Model model){
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList",questionList);
+        return "question_list";
     }
 }
