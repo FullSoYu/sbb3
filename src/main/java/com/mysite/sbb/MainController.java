070643a3c74ced41fe5b5948c177d31172721f1d
FullSoYu <hey6410@naver.com>
2022-11-10 15:48:50 +0900

    Root URL Jump to Sptring Boot 2-08

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+
     @RequestMapping("/sbb")
     @ResponseBody
     public String index() {
