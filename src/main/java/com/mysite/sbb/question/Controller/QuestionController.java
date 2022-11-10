b551797530a1aa89066ea75aeddf5454cb3f2746
FullSoYu <hey6410@naver.com>
2022-11-10 16:56:50 +0900

    Question detail Jump to Spring Boot 2-10

--- src/main/java/com/mysite/sbb/question/Controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/Controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +22,12 @@ public class QuestionController {
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
+    }
+
+    @RequestMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
