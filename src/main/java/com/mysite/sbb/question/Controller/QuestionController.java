df384df72d22110b3bfb26879801c3740fb8ad43
FullSoYu <hey6410@naver.com>
2022-11-11 18:44:08 +0900

    Register an answer Jump to Spring Boot 2-15

--- src/main/java/com/mysite/sbb/question/Controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/Controller/QuestionController.java
@@ -1,13 +1,15 @@
 package com.mysite.sbb.question.Controller;
 
 import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionForm;
 import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -30,4 +32,19 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+    @PostMapping("create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
+
+    }
+
 }
